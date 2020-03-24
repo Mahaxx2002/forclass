d9f85f5264b68ca7e41c4b20997342e19159fd2a
Mahaxx2002 <adam_zsombor@t-online.hu>
2020-03-24 18:00:53 +0100

    elso commit

---
+++ AdamZsomborFeladat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamZsomborFeladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Ádám Zsombor");
+            Console.WriteLine("2002.12.05");
+            Console.WriteLine("Nyomjon Bármilyen billentyűt a bezáráshoz");
+            Console.ReadKey ();
+        }
+    }
+}
---
+++ ÁÁÁÁÁÁÁÁÁÁ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ÁÁÁÁÁÁÁÁÁÁ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ÁÁÁÁÁÁÁÁÁÁ")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2904863d-de76-4170-82ca-5fe0a41fa438")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
